662fa645a2822417c729c306f5783ebb2f47b54d
Laviel <fafansin@gmail.com>
2020-07-28 11:29:03 +0300

    clean up event handling and selected cat

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,68 @@
 (function(){
     const model = {
+        currentCat:null,
         cats:[{name:"garfield", image:"public/images/garfield.jpg", score:0},
             {name:"nermal", image:"public/images/nermal.jpg", score:0},
             {name:"lion", image:"public/images/lion.jpeg", score:0},
             {name:"jaga", image:"public/images/jaga.jpeg", score:0},
             {name:"pathro", image:"public/images/pathro.jpg", score:0}
-        ],
-        getCats:function(){
-            return model.cats;
-        },
-        getCat:function(name){
-            return model.cats.find(cat => cat.name === name);
-        },
-        addScore:function(name){
-            let cat = model.getCat(name);
-            cat.score++;
-        }
-
+        ]
     }
     const controller = {
         init:function(){
+            model.currentCat = model.cats[0];
             view.init();
         },
-        getCats:function(){
-            return model.getCats();
+        getCurrentCat:function(){
+            return model.currentCat;
         },
-        viewCat:function(e){
-            const cat = model.getCat(e.target.id);
-            if(cat != null){
-                view.renderItem(cat);
-            }
+        setCurrentCat:function(cat){
+            model.currentCat = cat
         },
-        addScore:function(e){
-            const catName = e.target.id ? e.target.id : e.target.parentElement.id;
-            if(catName){
-                model.addScore(catName);
-                view.renderItem(model.getCat(catName));
-            }
+        addScore:function(){
+            model.currentCat.score++;
+            view.renderItem();
         }
     }
     const view = {
         init:function(){
-            const catList = document.getElementById("catList");
-            const catView = document.getElementById("catView");
-            catList.addEventListener("click", controller.viewCat, false);
-            catView.addEventListener("click", controller.addScore, false);
+            this.catList = document.getElementById("catList");
+            this.catView = document.getElementById("catView");
+            this.catName = document.getElementById("catName");
+            this.catImage = document.getElementById("catImage");
+            this.catScore = document.getElementById("catScore");
+            
+            this.catImage.addEventListener("click", function(e){
+                controller.addScore();
+            })
 
             view.renderList();
+            view.renderItem();
         },
         renderList:function(){
-            let htmlStr = '';
+            catList.innerHTML = '';
+
             controller.getCats().forEach(cat => {
-                htmlStr += `<li><button id="${cat.name}">${cat.name}</button></li>`;
+                let li = document.createElement("li");
+                li.textContent = cat.name;
+
+                li.addEventListener("click", (function(cat){
+                    return function(){
+                        console.log("event triggered");
+                        controller.setCurrentCat(cat);
+                        view.renderItem();
+                    }
+                })(cat));
+                view.catList.appendChild(li);
             });
-            console.log(catList);
-            catList.innerHTML = htmlStr;
+            
         },
-        renderItem:function(cat){
-            catView.innerHTML = '';
-            let htmlStr = '';
-            htmlStr += `<div id="${cat.name}" class="cat card">
-                            <h1>${cat.name}</h1>
-                            <img src="${cat.image}" alt="${cat.name}">
-                            <h2>${cat.score}</h2>
-                        </div>`;
-            catView.innerHTML = htmlStr;
+        renderItem:function(){
+            let cat = controller.getCurrentCat();
+            console.log(cat);
+            catName.textContent = cat.name;
+            catImage.src = cat.image;
+            catScore.textContent = cat.score;
         }
     }
 
